983b443d5167347df17412f9fc538b9611ee4fe4
Eric Vincent <eric@efvincent.com>
2021-08-19 15:59:29 -0400

    tab size 2 spaces

--- java/com/craftinginterpreters/lox/Lox.java
+++ java/com/craftinginterpreters/lox/Lox.java
@@ -12,55 +12,57 @@ import java.util.List;
 
 public class Lox {
 
-    private static final String TEST_FILE = "/home/efvincent/code/crafting-interpreters/java/com/craftinginterpreters/lox/test.lox";
-    public static boolean hadError = false;
-    
-    public static void main(String[] args) throws IOException {
-        if (args.length < 1) {
-            runFile(TEST_FILE);
-            // System.out.println("Usage: jlox [script]");
-            //System.exit(64);
-        } else if (args.length == 1) {
-            runFile(args[0]);
-        } else {
-            runPrompt();
-        }
-    }
+  private static final String TEST_FILE = "/home/efvincent/code/crafting-interpreters/java/com/craftinginterpreters/lox/test.lox";
+  public static boolean hadError = false;
 
-    private static void runFile(String path) throws IOException {
-        byte[] bytes = Files.readAllBytes(Paths.get(path));
-        run(new String(bytes, Charset.defaultCharset()));
-        if (hadError) System.exit(65);
+  public static void main(String[] args) throws IOException {
+    if (args.length < 1) {
+      runFile(TEST_FILE);
+      // System.out.println("Usage: jlox [script]");
+      // System.exit(64);
+    } else if (args.length == 1) {
+      runFile(args[0]);
+    } else {
+      runPrompt();
     }
+  }
 
-    private static void runPrompt() throws IOException {
-        InputStreamReader input = new InputStreamReader(System.in);
-        BufferedReader reader = new BufferedReader(input);
-        for(;;) {
-            System.out.print("> ");
-            String line = reader.readLine();
-            if (line == null) break;
-            run(line);
-            hadError = false;
-        }
+  private static void runFile(String path) throws IOException {
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.defaultCharset()));
+    if (hadError)
+      System.exit(65);
+  }
+
+  private static void runPrompt() throws IOException {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+    for (;;) {
+      System.out.print("> ");
+      String line = reader.readLine();
+      if (line == null)
+        break;
+      run(line);
+      hadError = false;
     }
+  }
 
-    private static void run(String source) {
-        Scanner scanner = new Scanner(source);
-        List<Token> tokens = scanner.scanTokens();
+  private static void run(String source) {
+    Scanner scanner = new Scanner(source);
+    List<Token> tokens = scanner.scanTokens();
 
-        // for now, just print the tokens
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+    // for now, just print the tokens
+    for (Token token : tokens) {
+      System.out.println(token);
     }
+  }
 
-    static void error(int line, String message) {
-        report(line, "", message);
-    }
+  static void error(int line, String message) {
+    report(line, "", message);
+  }
 
-    private static void report(int line, String where, String message) {
-        System.err.println("[line " + line +"] Error " + where + ": " + message);
-        hadError = true;
-    }
+  private static void report(int line, String where, String message) {
+    System.err.println("[line " + line + "] Error " + where + ": " + message);
+    hadError = true;
+  }
 }

ee4a6a83c56799dc27cdd8f7198dac6ea5768dce
Eric Vincent <eric@efvincent.com>
2021-08-22 22:13:10 -0400

    complete the parser

--- fsharp/Parser.fs
+++ fsharp/Parser.fs
@@ -1,52 +1,34 @@
 module Flox.Parser
 
+// Suppress a warning relating to the initialization soundness of recursively defined bindings
+// that are created using the `makeRule` function below
+#nowarn "40"  
+
 open Flox.Tokens
 open Flox.Expressions
 
-let rec expression (tokens : Token list) : Result<Expr * Token list,FloxError * Token list> =
-  raise (System.NotImplementedException())
+let private makeRule nextRule matchTypes =
+  fun tokens ->
+    let rec loop result =
+      result
+      |> Result.bind (fun (expr, rest) ->
+          match rest with 
+          | (t::rest') when List.contains t.Type matchTypes ->
+            nextRule rest'
+            |> Result.map  (fun (right, rest'') -> Binary (expr, t, right), rest'' ) 
+            |> loop
+          | _ -> Ok (expr, rest)
+        )
+    nextRule tokens
+    |> loop
 
-and equality tokens =
-  comparison tokens
-  |> Result.bind (fun (expr, rest) ->
-    match rest with
-    | (t::rest') when List.contains t.Type [BANG_EQUAL;EQUAL_EQUAL] ->
-      comparison rest'
-      |> Result.map (fun (right, rest'') -> Binary (expr, t, right), rest'')
-    | _ -> Ok (expr, rest)
-  )
+let rec private expression = equality
+and private equality =   makeRule comparison [BANG_EQUAL; EQUAL_EQUAL]
+and private comparison = makeRule term       [GREATER; GREATER_EQUAL; LESS; LESS_EQUAL] 
+and private term   =     makeRule factor     [MINUS;PLUS] 
+and private factor =     makeRule unary      [SLASH;STAR] 
 
-and comparison tokens =
-  term tokens
-  |> Result.bind (fun (expr, rest) ->
-    match rest with
-    | (t::rest') when List.contains t.Type [GREATER;LESS;GREATER_EQUAL;LESS_EQUAL] ->
-      term rest'
-      |> Result.map (fun (right, rest'') -> Binary (expr, t, right), rest'')
-    | _ -> Ok (expr, rest)
-  )
-
-and term tokens =
-  factor tokens
-  |> Result.bind (fun (expr, rest) ->
-    match rest with
-    | (t::rest') when List.contains t.Type [MINUS;PLUS] ->
-      factor rest'
-      |> Result.map (fun (right, rest'') -> Binary (expr, t, right), rest'')
-    | _ -> Ok (expr, rest)
-  )
-
-and factor tokens = 
-  unary tokens
-  |> Result.bind (fun (expr,rest) ->
-    match rest with
-    | (t::rest') when List.contains t.Type [SLASH;STAR] ->
-      unary rest'
-      |> Result.map (fun (right, rest'') -> Binary (expr, t, right), rest'')
-    | _ -> Ok (expr, rest)
-  )
-
-and unary tokens : Result<Expr * Token list, FloxError * Token list>=
+and private unary tokens =
   match tokens with
   | (tkn::tkns) ->
     match tkn.Type with
@@ -57,7 +39,7 @@ and unary tokens : Result<Expr * Token list, FloxError * Token list>=
     | _ -> primary tokens
   | [] -> Error ({Line=0; Msg="No tokens at unary"}, [])
 
-and primary tokens : Result<Expr * Token list, FloxError * Token list> =
+and private primary tokens = 
   match tokens with
   | (tkn::tkns) ->
     match tkn.Type with    
@@ -72,8 +54,13 @@ and primary tokens : Result<Expr * Token list, FloxError * Token list> =
         match rest with 
         | (t::rest') when t.Type = RIGHT_PAREN -> Ok (Grouping expr, rest')
         | (t::rest') -> Error (FloxError.FromToken t "Expect ')' after expression", rest')
-        | [] -> Error ({Line=0; Msg="ASSERTION-FAILED: Unexpected end of file" }, [])
+        | [] -> Error (FloxError.FromToken tkn "Expect ')' after expression", tkns)
       )
     | _ -> Error ((FloxError.FromToken tkn "Expecting expression"), tkns)
 
   | [] -> Error ({Line=0; Msg="No tokens at primary"}, [])
+
+let parse tokens =
+  expression tokens 
+  |> Result.mapError fst 
+  |> Result.map fst

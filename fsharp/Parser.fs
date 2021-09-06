ba33c599449c3d2bbcbf47e850ed1a43fa2ada7b
Eric Vincent <eric@efvincent.com>
2021-09-06 12:59:33 -0400

    update grammer for global vars

--- fsharp/Parser.fs
+++ fsharp/Parser.fs
@@ -12,18 +12,37 @@ open Flox.ErrorHandling
 (*
 grammar for flox
 
-program     -> statement* EOF ;
-statement   -> exprStmt | printStmt ;
+program     -> declaration* EOF ;
+
+declaration -> varDecl 
+              | statement;
+
+varDecl     -> "var" IDENTIFIER ( "=" expression )? ";" ;
+
+statement   -> exprStmt 
+              | printStmt ;
+
 exprStmt    -> expression ";" ;
+
 printStmt   -> "print" expression ";" ;
+
 expression  -> equality ;
+
 equality    -> comparison ( ( "!=" | "==") comparison )* ;
+
 comparison  -> term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
+
 term        -> factor ( ( "-" | "*" ) factor )* ;
+
 factor      -> unary ( ( "/" | "*" ) unary )* ;
-unary       -> ( "!" | "-" ) unary | primary ;
-primary     -> NUMBER | STRING | "true" | "false" | "nil"
-            | "(" expression ")" ;
+
+unary       -> ( "!" | "-" ) unary 
+              | primary ;
+
+primary     -> NUMBER | STRING 
+              | "true" | "false" | "nil" 
+              | "(" expression ")"
+              | IDENTIFIER ;
 
 *)
 

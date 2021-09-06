aab61c502f88cf89c440c24d8f719dd5fce3eecc
Eric Vincent <eric@efvincent.com>
2021-09-05 21:25:35 -0400

    override to string for expressions

--- fsharp/Expr.fs
+++ fsharp/Expr.fs
@@ -1,12 +1,36 @@
 module Flox.Expressions
 
 open Flox.Tokens
-
+open Flox.Lib
+open System.Text
 type Value  = Str of string
             | Num of float
             | Bool of bool
             | Nil
+with 
+  override this.ToString () =
+    match this with
+    | Str s -> s
+    | Num n -> string n 
+    | Bool b -> string b 
+    | Nil -> "nil"
 type Expr = Binary of Expr * Token * Expr
           | Grouping of Expr
           | Literal of Value
           | Unary of Token * Expr
+
+let rec private paren name (exprs: Expr list) =
+  let sb = StringBuilder()
+  sbAppend "( " sb  
+  |> sbAppend name 
+  |> ignore
+  exprs 
+  |> List.iter (fun e -> sbAppend " " sb |> sbAppend (exprToString e) |> ignore)
+  sbAppend ")" sb |> ignore
+  sb.ToString ()
+
+and exprToString = function
+| Literal ob             -> (string ob)
+| Unary (token,rhs)      -> paren token.Lexeme [rhs]
+| Grouping expr          -> paren "group" [expr]
+| Binary (lhs,token,rhs) -> paren token.Lexeme [lhs;rhs]

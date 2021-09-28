8c32ae283f5b7986f414b64fb899c68c20504cef
Eric Vincent <eric@efvincent.com>
2021-09-27 20:04:40 -0400

    better naming for upsert var in env

--- fsharp/Env.fs
+++ fsharp/Env.fs
@@ -4,8 +4,11 @@ open Flox.Expressions
 
 type Env = Map<string, Value>
 
-let defineVar (e:Env) n v : Env =
+/// Given an environment, a name, and a value, stores (or replaces)
+/// the value with the name in the environment
+let upsertVariable (e:Env) n v : Env =
   Map.add n v e
   
+/// Retrieves a value with a name `n` from the environment
 let getValue (e:Env) n : Value option =
   Map.tryFind n e
